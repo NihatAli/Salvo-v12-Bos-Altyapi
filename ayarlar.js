792b87bcfc8ac7e593013e94922887ebfdcbc9f8
Nikhat047 <66666800+NihatAli@users.noreply.github.com>
2022-11-02 12:47:49 +0400

    Update ayarlar.js

--- ayarlar.js
+++ ayarlar.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "token": "BOTUNUZUN TOKENİ",
-  "sahip": "SAHİP ID",
-  "prefix": "BOTUNUZUN PREFİXİ"
+  "token": "MTAzNDUxNDM0ODIwMDE3NzY3NQ.GV3JaI.u24I7ddupjm7eV-6styiWP5AGwYudEKAoguRYM",
+  "sahip": "719117042904727635",
+  "prefix": "!"
 };
